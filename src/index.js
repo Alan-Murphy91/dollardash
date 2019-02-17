2481b8becbc7c007bc00e20be01de1545f22bdd8
Alan <alanjmurphy91@gmail.com>
2019-02-17 12:58:28 +0000

    update

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom';
 
 import Form from './containers/Form';
 
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
 
 ReactDOM.render(<Form />, document.getElementById('app'));
