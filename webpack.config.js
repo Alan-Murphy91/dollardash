3b2eb45f5169cd9698484c14069ef299255c7f8e
Alan <alanjmurphy91@gmail.com>
2019-03-05 20:33:20 +0000

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,36 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-      },
-      {
-        test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
+  	module: {
+		rules: [
+		{
+			test: /\.(js|jsx)$/,
+			exclude: /node_modules/,
+			use: {
+			loader: "babel-loader"
+			}
+		},
+		{
+			test: /\.html$/,
+			use: [
+			{
+				loader: "html-loader"
+			}
+			]
+		},
+		{
+			test: /\.s?css$/,
+			use: [
+			'style-loader',
+			'css-loader',
+			'sass-loader'
+			]
+		}
+		]
+  	},
+	devServer: {
+		historyApiFallback: true,
+	},
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
