acd735a81cd8b3172de1c8b3a75f2c110a9e0611
Alan <alanjmurphy91@gmail.com>
2019-06-01 18:47:07 +0100

    update

--- src/redux/selectors/test/expenses.spec.js
+++ src/redux/selectors/test/expenses.spec.js
@@ -47,10 +47,28 @@ test('should filter by start date', () => {
 	);
 });
 
+test('should filter by end date', () => {
+	const filters = {
+		text: '',
+		sortBy: 'date',
+		startDate: undefined,
+		endDate: moment(0).add(2, 'days')
+	};
+	const result = selectExpenses(expenses, filters);
+	expect(result).toEqual(
+		[expenses[0], expenses[1]]
+	);
+});
 
-// filter by amount 
-
-// filter by end date
-
-
-// test will all examples
+test('should filter by amount', () => {
+	const filters = {
+		text: '',
+		sortBy: 'amount',
+		startDate: undefined,
+		endDate: undefined,
+	};
+	const result = selectExpenses(expenses, filters);
+	expect(result).toEqual(
+		[expenses[1], expenses[2], expenses[0]]
+	);
+});
